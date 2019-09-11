e3b0da9d9c51cdfaf0f62df01657914568727f72
Aldair <aldair.flores.moya@gmail.com>
2019-09-11 15:23:26 -0400

    Ingreso de datos iniciales en BD

--- src/main/java/NominaSueldo/CargarBaseDatos.java
+++ src/main/java/NominaSueldo/CargarBaseDatos.java
@@ -5,8 +5,15 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+
 @Configuration
 @Slf4j
 public class CargarBaseDatos {
-
+    @Bean
+    CommandLineRunner initDatabase(RepositorioRider repository) {
+        return args -> {
+            log.info("Preloading " + repository.save(new Rider("Alberto", "20", "6135091")));
+            log.info("Preloading " + repository.save(new Rider("Alan", "21", "6435082")));
+        };
+    }
 }
