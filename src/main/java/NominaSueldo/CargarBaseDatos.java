e0aa91bd81c7957314ddca07533f0d25780704b1
Aldair <aldair.flores.moya@gmail.com>
2019-09-11 14:50:42 -0400

    Creacion solicitud nomina y cargar BD

---
+++ src/main/java/NominaSueldo/CargarBaseDatos.java
@@ -0,0 +1,12 @@
+package NominaSueldo;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@Slf4j
+public class CargarBaseDatos {
+
+}
---
+++ src/main/java/NominaSueldo/SolicitudNomina.java
@@ -0,0 +1,12 @@
+package NominaSueldo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SolicitudNomina {
+
+    public static void main(String... args) {
+        SpringApplication.run(SolicitudNomina.class, args);
+    }
+}
