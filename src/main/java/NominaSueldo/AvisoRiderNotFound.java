cb47b05c00e7863c6432e96b8b9164a1c9eaeb6a
Aldair <aldair.flores.moya@gmail.com>
2019-09-12 10:18:04 -0400

    Clase para Mensaje de excepcion no encontrado

---
+++ src/main/java/NominaSueldo/AvisoRiderNotFound.java
@@ -0,0 +1,17 @@
+package NominaSueldo;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ControllerAdvice
+public class AvisoRiderNotFound {
+    @ResponseBody
+    @ExceptionHandler(RiderNotFoundException.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    String AvisoRiderNotFoundHandler(RiderNotFoundException ex) {
+        return ex.getMessage();
+    }
+}
