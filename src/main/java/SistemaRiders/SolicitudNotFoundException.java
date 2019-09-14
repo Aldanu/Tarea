5c00e9526c08ad8b8ae2400ceb2dfd76b471786b
Aldair <aldair.flores.moya@gmail.com>
2019-09-14 12:54:05 -0400

    Revert "Añadir funciones a controladorSolicitud y Manejo de errores Para solicitudes"
    Esto revierte commit ea5066e4

--- src/main/java/SistemaRiders/SolicitudNotFoundException.java
+++
@@ -1,8 +0,0 @@
-package SistemaRiders;
-
-public class SolicitudNotFoundException extends RuntimeException {
-
-    SolicitudNotFoundException(Long id) {
-        super("No se logro encontrar la solicitud " + id);
-    }
-}
